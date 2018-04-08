fd983c2a30d6eb46dbcbe47f546d1b7d09368363
Bao Nguyen The Vinh <bao.nguyen150028@gmail.com>
2018-04-08 13:54:51 +0900

    Finished auth flow

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,7 +7,13 @@ module.exports = (app) => {
     );
     app.get('/auth/google/callback',passport.authenticate('google'))
     
+    app.get('/api/logout', (req, res) => {
+        req.logout();
+        res.send('You already logged out');
+    })
+
     app.get('/api/current_user', (req, res) => {
         res.send(req.user);
     });
+    
 };
